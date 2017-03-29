aed431e43e697fceca6a8027a228c3ec45961513
Administrator <Administrator@XZ-20160920UOKQ>
2017-03-29 08:32:38 +0800

    1

--- src/main/java/com/lxr/nvc/utils/ParameterUtils.java
+++ src/main/java/com/lxr/nvc/utils/ParameterUtils.java
@@ -16,75 +16,92 @@ import com.lxr.nvc.annotation.Param;
 import com.lxr.nvc.model.Model;
 
 public class ParameterUtils {
-		private static Object[] objs ;
-		public static <T> Object[] buildParams(HttpRequestUtil requestUtil,Parameter[] parameters){
-			try {
-				objs = new Object[parameters.length];
- 				for(int i=0;i<parameters.length;i++){
-					Parameter parameter	=parameters[i];
-//					Type parameterizedType = parameter.getParameterizedType();// 获取带泛型的形参类型
-					Class<T> type = (Class<T>) parameter.getType();
-					Type parameterizedType = parameter.getParameterizedType();
-					if(type.equals(HttpServletRequest.class)){
-						objs[i]=requestUtil.getRequest();
-					}
-					else if(type.equals(HttpServletResponse.class)){
-						objs[i]=requestUtil.getResponse();
-					}
-					else if(type.equals(HttpSession.class)){
-						objs[i]=requestUtil.getSession();
-					}
-					else if(type.equals(Model.class)){
-						objs[i]=requestUtil.getModel();
-					}
-					else if(type.isPrimitive() ){
-						Param annotation = parameter.getAnnotation(Param.class);
-						
-						if(type.equals(int.class)){
-							objs[i]=Integer.parseInt(requestUtil.getRequest().getParameter(annotation.value()));
-						}
-						else if( type.equals(double.class)){
-							objs[i]=Double.parseDouble(requestUtil.getRequest().getParameter(annotation.value()));
-						}
-						else if(type.equals(Date.class) ){
-							objs[i]=new SimpleDateFormat("yyyyMMddHHmmss").parse(requestUtil.getRequest().getParameter(annotation.value()));
-						}
-						else if(type.equals(boolean.class)){
-							objs[i]=Boolean.parseBoolean(requestUtil.getRequest().getParameter(annotation.value()));
-						}
-						
-					}
-					
-					else if(type.equals(String.class)){
-						Param annotation = parameter.getAnnotation(Param.class);
-						objs[i]=requestUtil.getRequest().getParameter(annotation.value());
-					}
-					else if(type.equals(Double.class)){
-						Param annotation = parameter.getAnnotation(Param.class);
-						objs[i]=Double.parseDouble(requestUtil.getRequest().getParameter(annotation.value()));
-					}
-					else if(type.equals(Date.class)){
-						Param annotation = parameter.getAnnotation(Param.class);
-						objs[i]=new SimpleDateFormat("yyyyMMddHHmmss").parse(requestUtil.getRequest().getParameter(annotation.value()));
-					}
-					else if(type.equals(Boolean.class)){
-						Param annotation = parameter.getAnnotation(Param.class);
-						objs[i]=Boolean.parseBoolean(requestUtil.getRequest().getParameter(annotation.value()));
-					}
-					else if(type.equals(Integer.class)){
-						Param annotation = parameter.getAnnotation(Param.class);
+	private static Object[] objs ;
+	public static <T> Object[] buildParams(HttpRequestUtil requestUtil,Parameter[] parameters){
+		try {
+			objs = new Object[parameters.length];
+			for(int i=0;i<parameters.length;i++){
+				Parameter parameter	=parameters[i];
+				//					Type parameterizedType = parameter.getParameterizedType();// 获取带泛型的形参类型
+				Class<T> type = (Class<T>) parameter.getType();
+				Type parameterizedType = parameter.getParameterizedType();
+				if(type.equals(HttpServletRequest.class)){
+					objs[i]=requestUtil.getRequest();
+				}
+				else if(type.equals(HttpServletResponse.class)){
+					objs[i]=requestUtil.getResponse();
+				}
+				else if(type.equals(HttpSession.class)){
+					objs[i]=requestUtil.getSession();
+				}
+				else if(type.equals(Model.class)){
+					objs[i]=requestUtil.getModel();
+				}
+				else if(type.isPrimitive() ){
+					Param annotation = parameter.getAnnotation(Param.class);
+
+					if(type.equals(int.class)){
 						objs[i]=Integer.parseInt(requestUtil.getRequest().getParameter(annotation.value()));
 					}
-					else { //对象的处理
-						T t=BeanUtils.getBean(type, requestUtil);
-						objs[i]=t;
+					else if( type.equals(double.class)){
+						objs[i]=Double.parseDouble(requestUtil.getRequest().getParameter(annotation.value()));
 					}
+					else if(type.equals(Date.class) ){
+						objs[i]=new SimpleDateFormat("yyyyMMddHHmmss").parse(requestUtil.getRequest().getParameter(annotation.value()));
+					}
+					else if(type.equals(boolean.class)){
+						objs[i]=Boolean.parseBoolean(requestUtil.getRequest().getParameter(annotation.value()));
+					}
+
 				}
+
+				else if(type.equals(String.class)){
+					Param annotation = parameter.getAnnotation(Param.class);
+					if(annotation.value()!=null && !annotation.value().equals("")){
+						objs[i]=requestUtil.getRequest().getParameter(annotation.value());
+					}
 				
-			} catch (Exception e) {
-				// TODO: handle exception
+
+				}
+				else if(type.equals(Double.class)){
+
+					Param annotation = parameter.getAnnotation(Param.class);
+					if(annotation.value()!=null && !annotation.value().equals("")){
+						objs[i]=Double.parseDouble(requestUtil.getRequest().getParameter(annotation.value()));
+					}
+					
+				}
+				else if(type.equals(Date.class)){
+					Param annotation = parameter.getAnnotation(Param.class);
+					if(annotation.value()!=null && !annotation.value().equals("")){
+						objs[i]=new SimpleDateFormat("yyyyMMddHHmmss").parse(requestUtil.getRequest().getParameter(annotation.value()));
+					}
+					
+				}
+				else if(type.equals(Boolean.class)){
+					Param annotation = parameter.getAnnotation(Param.class);
+					if(annotation.value()!=null && !annotation.value().equals("")){
+						objs[i]=Boolean.parseBoolean(requestUtil.getRequest().getParameter(annotation.value()));
+					}
+					
+				}
+				else if(type.equals(Integer.class)){
+					Param annotation = parameter.getAnnotation(Param.class);
+					if(annotation.value()!=null && !annotation.value().equals("")){
+						objs[i]=Integer.parseInt(requestUtil.getRequest().getParameter(annotation.value()));
+					}
+				
+				}
+				else { //对象的处理
+					T t=BeanUtils.getBean(type, requestUtil);
+					objs[i]=t;
+				}
 			}
-			return objs;
-			
+
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
+		return objs;
+
+	}
 }
